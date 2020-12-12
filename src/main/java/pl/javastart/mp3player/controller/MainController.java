290b3d62e738402274b97294cead80591b9f52fe
ewa0601 <egmu31@gmail.com>
2020-12-12 18:16:44 +0100

    Add multi view and more controller classes

--- src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -1,64 +1,10 @@
 package pl.javastart.mp3player.controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
-    @FXML
-    private MenuItem fileMenuItem;
-
-    @FXML
-    private MenuItem dirMenuItem;
-
-    @FXML
-    private MenuItem closeMenuItem;
-
-    @FXML
-    private MenuItem aboutMenuItem;
-
-    @FXML
-    private TableView<?> contentTable;
-
-    @FXML
-    private Button previousButton;
-
-    @FXML
-    private ToggleButton playButton;
-
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private Slider volumeSlider;
-
-    @FXML
-    private Slider progressSlider;
-
     public void initialize() {
-        configureButtons();
-        configureVolume();
-    }
-
-    private void configureButtons() {
-        previousButton.setOnAction(event -> System.out.println("Poprzednia piosenka"));
-        nextButton.setOnAction(event -> System.out.println("Następna piosenka"));
-        playButton.setOnAction(event -> {
-            if (playButton.isSelected())
-                System.out.println("Play");
-            else
-                System.out.println("Stop");
-        });
-    }
-
-    private void configureVolume() {
-        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED,
-                mouseEvent -> System.out.println("Wciśnięto przycisk na suwaku głośności"));
+        System.out.println("Main controller created");
     }
 
 }
